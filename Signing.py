23e076301b85d84666abd27802f016e60f1d2cd7
farhan3001 <74050546+farhan3001@users.noreply.github.com>
2023-04-13 23:35:01 +0700

    Add files via upload

---
+++ Signing.py
@@ -0,0 +1,175 @@
+import random
+import math
+from sympy import *
+import sympy.ntheory as nt
+import hashlib
+
+class Signing():
+
+    def getMessageInSignedFile(self, path):
+        selectedElements = []
+        with open(path, "r") as f:
+            for line in f:
+                if '<d>' not in line:
+                    selectedElements.append(line)
+
+        selectedElements.pop()
+        removeSpacing = selectedElements[-1].replace("\n","")
+        selectedElements.pop()
+        selectedElements.append(removeSpacing)
+        f.close()
+
+        result = ''
+        currentGroup = ''
+
+        for line in selectedElements:
+            if line == '\n':
+                if currentGroup:
+                    result += currentGroup.strip() + '\n\n'
+                    currentGroup = ''
+            else:
+                currentGroup += line
+
+        if currentGroup:
+            result += currentGroup.strip()
+
+        return (result)    
+
+    def getSignInSignedFile(self, path):
+        sign = ""
+        char1 = '<d> '
+        char2 = ' </d>'
+
+        with open(path, "r") as f:
+            for line in f:
+                if '<d>' in line:
+                    sign = line
+        
+        result = sign[sign.find(char1)+4 : sign.find(char2)]
+        return result
+
+    def writeSignInFile(self,path, messageSigned, message):
+        check = False
+        file = open("signed_message.txt", "w+")
+        file.writelines(message)
+        file.close()
+
+        with open(path, 'r') as f:
+            lines = f.readlines()
+
+        for i, line in enumerate(lines):
+            if line.startswith("<d>"):
+                lines[i] = messageSigned
+                check = True
+                break
+
+        if check == True:
+            with open(path, 'w') as f:
+                f.writelines(lines)
+        else:
+            file = open(path, "a+")
+            file.seek(0)
+            file.write("\n"+"\n"+messageSigned)
+    
+    def gcd(self,a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a  
+
+    def extendedGcd(self,a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            gcd, x, y = self.extendedGcd(b % a, a)
+            return (gcd, y - (b // a) * x, x)
+        
+    def primeGenerator(self):
+        while True:
+            result = random.randint(0, 100000000)
+            if nt.isprime(result):
+                return result
+            
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+        
+    def generateKeyPair(self):
+        p  = self.primeGenerator()
+        q = self.primeGenerator()
+
+        n = p * q 
+        phi = (p-1) * (q-1)
+        e = random.randrange(1, phi)
+
+        g = gcd(e, phi)
+        
+        while (g != 1):
+            e = random.randrange(1, phi)
+            g = gcd(e, phi)
+
+        d = self.extendedGcd(e, phi)[1]
+
+        return ((e, n), (d, n))
+    
+    def encrypt(self, plaintext, privateKey):
+        d, n = privateKey
+        sizeBlock = math.ceil(n.bit_length() / 8)
+        plainBlocks = [bytes.fromhex('00') + plaintext[i:i+sizeBlock-1] for i in range(0, len(plaintext), sizeBlock-1)]
+
+        padLength = sizeBlock - len(plainBlocks[-1])
+        if padLength:
+            plainBlocks[-1] = bytes.fromhex('00') * padLength + plainBlocks[-1]
+
+        plainBlocksCombine = [int.from_bytes(byte, byteorder='big') for byte in plainBlocks]
+
+        cipherBlocks = [pow(block, d, n) for block in plainBlocksCombine]
+        cipherBlocksCombine = [block.to_bytes(length=sizeBlock, byteorder='big') for block in cipherBlocks]
+
+        ciphertext = b''.join(cipherBlocksCombine)
+        ciphertext += padLength.to_bytes(length=4, byteorder='big')
+
+        return ciphertext.hex()
+
+    def decrypt(self,ciphertext, publicKey):
+        e, n = publicKey
+        blocksize = (n.bit_length() + 7) // 8
+
+        cipherBlocks, padding = ciphertext[:-4], int.from_bytes(ciphertext[-4:], byteorder='big')
+        cipherBlocks = [int.from_bytes(cipherBlocks[i:i+blocksize], byteorder='big') for i in range(0, len(cipherBlocks), blocksize)]
+
+        plainBlocks = [pow(c, e, n).to_bytes(length=blocksize, byteorder='big') for c in cipherBlocks]
+        plainBlocks[-1] = plainBlocks[-1][padding:]
+
+        plaintext = b''.join(block[1:] for block in plainBlocks)
+
+        return plaintext.hex()
+        
+    def sha3(self,message):
+        hashedMessage = hashlib.sha3_256(message.encode("latin-1")).hexdigest()
+        return hashedMessage
+
+    def generateSignMessage(self, path, privateKey):
+        with open(path, "r") as f:
+            message = f.read()
+        
+        messageDigest = self.sha3(message)
+        messageDigest = bytes.fromhex(messageDigest)
+        sign = self.encrypt(messageDigest, privateKey)
+
+        writtenSigned = "<d> " + sign + " </d>"
+
+        self.writeSignInFile("signed_message.txt",writtenSigned, message)
+        
+        return sign 
+    
+    def validateSignedMessage(self,path, publicKey):
+        message = self.getMessageInSignedFile(path)
+        sign = self.getSignInSignedFile(path)
+
+        signHex = bytes.fromhex(sign)
+
+        return self.sha3(message) == self.decrypt(signHex, publicKey)
---
+++ main.py
@@ -0,0 +1,285 @@
+import os
+import sys
+import tkinter as tkinter
+from tkinter import filedialog
+from Signing import *
+
+class MainWindow:
+
+    # configure root directory path relative to this file
+    
+    THIS_FOLDER_G = ""
+    if getattr(sys, "frozen", False):
+        THIS_FOLDER_G = os.path.dirname(sys.executable)
+    else:
+        THIS_FOLDER_G = os.path.dirname(os.path.realpath(__file__))
+
+    def __init__(self, root):
+        self.root = root
+        self.fileUrl = tkinter.StringVar()
+        self.publicKeyGenerated = tkinter.StringVar()
+        self.status = tkinter.StringVar()
+        self.status.set("---")
+
+        self.shouldCancel = False
+
+        root.title("Digital Signing")
+        root.configure(bg="#FFFDD0")
+
+        self.fileEntryLabel = tkinter.Label(
+            root,
+            text="Enter File Message Path Or Click SELECT FILE Button",
+            bg="#FFFDD0",
+            fg="#000000",
+            anchor=tkinter.W
+        )
+        self.fileEntryLabel.grid(
+            padx=12,
+            pady=(8, 0),
+            ipadx=0,
+            ipady=1,
+            row=0,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.fileEntry = tkinter.Entry(
+            root,
+            textvariable=self.fileUrl,
+            bg="#fff",
+            exportselection=0,
+            relief=tkinter.FLAT
+        )
+        self.fileEntry.grid(
+            padx=15,
+            pady=6,
+            ipadx=8,
+            ipady=8,
+            row=1,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.selectBtn = tkinter.Button(
+            root,
+            text="SELECT FILE",
+            command=self.selectFileCallback,
+            width=42,
+            bg="#1089ff",
+            fg="#000000",
+            bd=2,
+            relief=tkinter.FLAT
+        )
+        self.selectBtn.grid(
+            padx=15,
+            pady=8,
+            ipadx=24,
+            ipady=6,
+            row=2,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.generatePublicKey = tkinter.Label(
+            root,
+            text="Generated Public Key:",
+            bg="#FFFDD0",
+            fg="#000000",
+            anchor=tkinter.W
+        )
+        self.generatePublicKey.grid(
+            padx=12,
+            pady=(8, 0),
+            ipadx=0,
+            ipady=1,
+            row=3,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.generatePublicKey = tkinter.Entry(
+            root,
+            bg="#fff",
+            textvariable=self.publicKeyGenerated,
+            exportselection=0,
+            state='disabled',
+            relief=tkinter.FLAT,
+        )
+        self.generatePublicKey.grid(
+            padx=15,
+            pady=6,
+            ipadx=8,
+            ipady=8,
+            row=4,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.generateBtn = tkinter.Button(
+            root,
+            text="GENERATE",
+            command=self.generateCallback,
+            bg="#ed3833",
+            fg="#000000",
+            bd=2,
+            relief=tkinter.FLAT
+        )
+        self.generateBtn.grid(
+            padx=(15, 6),
+            pady=8,
+            ipadx=24,
+            ipady=6,
+            row=5,
+            column=0,
+            columnspan=2,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+        
+        self.validateBtn = tkinter.Button(
+            root,
+            text="VALIDATE",
+            command=self.validateCallback,
+            bg="#00bd56",
+            fg="#000000",
+            bd=2,
+            relief=tkinter.FLAT
+        )
+        self.validateBtn.grid(
+            padx=(6, 15),
+            pady=8,
+            ipadx=24,
+            ipady=6,
+            row=5,
+            column=2,
+            columnspan=2,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.resetBtn = tkinter.Button(
+            root,
+            text="RESET",
+            command=self.resetCallback,
+            bg="#aaaaaa",
+            fg="#000000",
+            bd=2,
+            relief=tkinter.FLAT
+        )
+        self.resetBtn.grid(
+            padx=15,
+            pady=(4, 12),
+            ipadx=24,
+            ipady=6,
+            row=6,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+        self.statusBtn = tkinter.Label(
+            root,
+            textvariable=self.status,
+            bg="#FFFDD0",
+            fg="#000000",
+            anchor=tkinter.W,
+            justify=tkinter.LEFT,
+            relief=tkinter.FLAT,
+            wraplength=350
+        )
+        self.statusBtn.grid(
+            padx=12,
+            pady=(0, 12),
+            ipadx=0,
+            ipady=1,
+            row=7,
+            column=0,
+            columnspan=4,
+            sticky=tkinter.W+tkinter.E+tkinter.N+tkinter.S
+        )
+
+    def selectFileCallback(self):
+        try:
+            name = filedialog.askopenfile()
+            self.fileUrl.set(name.name)
+    
+        except Exception as e:
+            self.status.set(e)
+            self.statusBtn.update()       
+    
+    def freezeControls(self):
+        self.fileEntry.configure(state="disabled")
+        self.selectBtn.configure(state="disabled")
+        self.generateBtn.configure(state="disabled")
+        self.validateBtn.configure(state="disabled")
+        self.resetBtn.configure(text="CANCEL", command=self.cancelCallback,
+            fg="#ed3833", bg="#fafafa")
+        self.statusBtn.update()
+    
+    def unfreezeControls(self):
+        self.fileEntry.configure(state="normal")
+        self.selectBtn.configure(state="normal")
+        self.generateBtn.configure(state="normal")
+        self.validateBtn.configure(state="normal")
+        self.resetBtn.configure(text="RESET", command=self.resetCallback,
+            fg="#ffffff", bg="#aaaaaa")
+        self.statusBtn.update()
+
+    def generateCallback(self):
+        self.freezeControls()
+        self.status.set("---")
+        try:
+            signing = Signing()
+
+            path = self.fileEntry.get()
+            publicKey, privateKey = signing.generateKeyPair()
+            # print(publicKey)
+            signing.generateSignMessage(path, privateKey)
+            self.publicKeyGenerated.set(publicKey)
+
+        except Exception as e:
+            self.status.set(e)
+        
+        self.unfreezeControls()    
+
+    def validateCallback(self):
+        self.freezeControls()
+
+        try:
+            signing = Signing()
+
+            path = self.fileEntry.get()
+            publicKey = self.publicKeyGenerated.get()
+
+            # print(publicKey)
+            
+            publicKeyTupple = tuple(map(int, publicKey[1:-1].split(',')))
+
+            # print(publicKeyTupple)
+
+            if signing.validateSignedMessage(path, publicKeyTupple) == True:
+                self.status.set("File: Authentic")
+            else:
+                self.status.set("File: False File")    
+
+        except Exception as e:
+            self.status.set(e)
+        
+        self.unfreezeControls()
+
+    def resetCallback(self):
+        self.fileUrl.set("")
+        self.publicKeyGenerated.set("")
+        self.status.set("---")
+    
+    def cancelCallback(self):
+        self.shouldCancel = True
+
+
+if __name__ == "__main__":
+    ROOT = tkinter.Tk()
+    MAIN_WINDOW = MainWindow(ROOT)
+    ROOT.mainloop()
